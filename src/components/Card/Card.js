1a21a1086044966253b30667e874004896a348cb
Nastassia Haradzetskaya <55383793+NHaradetskaya@users.noreply.github.com>
2021-01-14 11:57:02 +0300

    Task3 (#3)
    * Task3
    
    * task3 v2
    
    * task3 v3
    
    Co-authored-by: Nastassia Haradzetskaya <nastya10gorodetskaya@gmail.com>

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,35 +1,119 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './Card.css';
+import {
+    AiFillEdit,
+    AiFillDownCircle,
+    AiFillCloseCircle,
+} from 'react-icons/ai';
 
-const Card = (props) => {
-    const [flag, setFlag] = useState(false);
+class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            flagCheck: false,
+            editCheck: false,
+            title: this.props.name,
+            text: this.props.children,
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-    const switchColor = () => setFlag(!flag);
+    switchColor = () => {
+        this.setState({ flagCheck: !this.state.flagCheck });
+    };
 
-    return (
-        <div
-            className="Section"
-            style={{
-                backgroundColor: flag ? 'green' : 'white',
-                color: flag ? 'white' : 'black',
-            }}
-        >
-            <div className="Section__title">
-                <h3>{props.name}</h3>
-                <input
-                    className="Checkbox"
-                    type="checkbox"
-                    checked={flag}
-                    onChange={switchColor}
-                />
+    edit = () => {
+        this.setState({ editCheck: true, flagCheck: false });
+    };
+
+    cancel = () => {
+        this.setState({
+            editCheck: !this.state.editCheck,
+            flagCheck: false,
+            title: this.props.name,
+            text: this.props.children,
+        });
+    };
+
+    save = () => {
+        this.setState({
+            editCheck: false,
+            flagCheck: false,
+        });
+    };
+
+    handleChange = (event) => {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    render() {
+        const { flagCheck, editCheck, title, text } = this.state;
+        return (
+            <div
+                className="section"
+                style={{
+                    backgroundColor: flagCheck ? 'green' : 'white',
+                    color: flagCheck ? 'white' : 'black',
+                }}>
+                <div className="section__title">
+                    {!editCheck ? (
+                        <div className="title__block">
+                            <h3 className="title">{title}</h3>
+                            <div>
+                                <input
+                                    className="checkbox"
+                                    type="checkbox"
+                                    checked={flagCheck}
+                                    onChange={this.switchColor}
+                                />
+                                <button
+                                    onClick={this.edit}
+                                    className="btn__edit">
+                                    <AiFillEdit />
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="title__block">
+                            <textarea
+                                name="title"
+                                value={title}
+                                onChange={this.handleChange}
+                            />
+                            <div>
+                                <button
+                                    onClick={this.save}
+                                    className="btn__save">
+                                    <AiFillDownCircle /> Save
+                                </button>
+                                <button
+                                    onClick={this.cancel}
+                                    className="btn__cancel">
+                                    <AiFillCloseCircle /> Cancel
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                </div>
+
+                <div className="line" />
+                <div className="section__text">
+                    {!editCheck ? (
+                        <p>{text}</p>
+                    ) : (
+                        <textarea
+                            name="text"
+                            value={text}
+                            onChange={this.handleChange}
+                        />
+                    )}
+                </div>
             </div>
-
-            <div className="Line" />
-            <div className="Section__text">
-                <p>{props.children}</p>
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Card;
